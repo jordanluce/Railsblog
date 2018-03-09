c810fac9c1d90f02c119c1a5c94760dd82877e2e
jordan luce <jordan.luce972@gmail.com>
2018-03-09 17:49:42 +0000

    Project completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+	http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
 def create
 	@article = Article.find(params[:article_id])
@@ -10,7 +11,7 @@ def destroy
 	@article = Article.find(params[:article_if])
 	@comment = @article.comments.find(params[:id])
 	@comment.destroy
-	
+
 	redirect_to article_path(@article)
 end
 
